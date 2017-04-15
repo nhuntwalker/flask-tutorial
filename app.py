9f32bfd0dbe24703b903507c5d8c3fb67ede02bd
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-04-15 12:08:38 -0700

    Added some views that have routes with variables. Variables from the route get passed into the view callable

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ def index() -> str:
 def hello() -> str:
     """Another page."""
     return "Hello world!"
+
+
+@app.route('/user/<username>')
+def show_user_profile(username: str) -> str:
+    """Given a username, show that user's profile."""
+    return f'{username}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id: int) -> str:
+    """Show the post with the given id."""
+    return f'Post {post_id}'
